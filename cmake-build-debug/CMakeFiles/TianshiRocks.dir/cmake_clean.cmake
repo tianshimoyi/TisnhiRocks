82086280e07e82ea99a94bfa2a6f1cb4e417f134
tianshimoyi <1574783084@qq.com>
2022-04-13 20:03:14 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/TianshiRocks.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TianshiRocks.dir/main.cpp.o"
+  "TianshiRocks"
+  "TianshiRocks.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TianshiRocks.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/TianshiRocks.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for TianshiRocks.
+# This may be replaced when dependencies are built.
